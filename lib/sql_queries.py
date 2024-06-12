b0e65ab669d86eff79f647b99c3b694669659642
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-12 23:49:56 +0300

    selecting all bears names and orders in aplhabetical order

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -8,7 +8,10 @@ select_all_female_bears_return_name_and_age = """
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY bears.name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
